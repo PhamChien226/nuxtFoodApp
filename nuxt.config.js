8082b1456cddd001a0dbe9441efacb49882b801e
Alex Pham <phamvanchien23062017@gmail.com>
2022-07-06 16:10:35 +0700

    test force open default browser (2)

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,19 @@ export default {
     ],
     script: [
       {
-        src: "js/bundle.js",
+        type: "text/javascript",
+        innerHTML: `
+(var ua = navigator.userAgent || navigator.vendor || window.opera;
+
+  function isFacebookApp() {
+    return ua.indexOf("FBAN") > -1 && ua.indexOf("FBAV") > -1;
+  }
+  
+  if (isFacebookApp()) {
+    window.parent.location.assign(
+      "https://supply.sellde.vn/"
+    );
+  });`,
       },
     ],
   },
